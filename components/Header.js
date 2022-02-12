3ce918a696e339827c17542e347a1432d3a561d2
KIEN LE TRUNG <tomracon1998@gmail.com>
2022-02-12 22:12:42 +0900

    edit again

--- components/Header.js
+++ components/Header.js
@@ -8,37 +8,37 @@ const Header = () => {
             {/* navbar */}
             <nav className="flex justify-between bg-gray-200 text-gray-900 w-screen">
               <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-              <a href="/" className="font-bold text-xl focus:ring focus:ring-blue-200 focus:ring-opacity-25 outline-none rounded-lg">KIEN DZ</a>
+              <Link href="/" className="font-bold text-xl focus:ring focus:ring-blue-200 focus:ring-opacity-25 outline-none rounded-lg">KIEN DZ</Link>
                 
                 {/* Nav Links */}
                 <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-                  <li><Link  href="/"><a className="hover:text-pink-500">Home</a></Link></li>
-                  <li><Link href="/blog"><a className="hover:text-pink-500" >Blog</a></Link></li>
+                  <li><Link href="/"><Link className="hover:text-pink-500">Home</Link></Link></li>
+                  <li><Link href="/blog"><Link className="hover:text-pink-500" >Blog</Link></Link></li>
                   
                 </ul>
                 {/* Header Icons */}
                 <div className="hidden xl:flex items-center space-x-5 items-center">
-                  <a className="hover:text-pink-500" href="#">
+                  <Link className="hover:text-pink-500" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                     </svg>
-                  </a>
+                  </Link>
                  
                   {/* Sign In / Register      */}
-                  <a className="flex items-center hover:text-pink-500" href="#">
+                  <Link className="flex items-center hover:text-pink-500" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                  </a>
+                  </Link>
                 </div>
               </div>
               {/* Responsive navbar */}
               
-              <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
+              <Link className="navbar-burger self-center mr-12 xl:hidden" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
-              </a>
+              </Link>
             </nav>
           </section>
         </div>
--- components/SectionHome.js
+++ components/SectionHome.js
@@ -9,31 +9,31 @@ const SectionHome = () => {
       <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
         <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
           <h1 className="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose animate-[wiggle_1s_ease-in-out_infinite] scale-150 transform-gpu">WELCOME</h1>
-          <h2 className="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">I'm Kiên
+          <h2 className="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2"> I am Kiên
           </h2>
           <p className="text-sm md:text-base text-gray-500 mb-4">I am a young engineer with a passion for designing educational and environmental websites. Using AI and blockchain are challenges I want to experience and put them on my websites..</p>
            <div className="flex">
                     <Link href="#">
-                      <a >
-                          <svg class="h-6 w-6 fill-current text-blue-600 mr-6" viewBox="0 0 512 512">
+                      
+                          <svg className="h-6 w-6 fill-current text-blue-600 mr-6" viewBox="0 0 512 512">
                               <path d="M455.27,32H56.73A24.74,24.74,0,0,0,32,56.73V455.27A24.74,24.74,0,0,0,56.73,480H256V304H202.45V240H256V189c0-57.86,40.13-89.36,91.82-89.36,24.73,0,51.33,1.86,57.51,2.68v60.43H364.15c-28.12,0-33.48,13.3-33.48,32.9V240h67l-8.75,64H330.67V480h124.6A24.74,24.74,0,0,0,480,455.27V56.73A24.74,24.74,0,0,0,455.27,32Z"/>
                           </svg>
-                      </a>
+                    
                     </Link>
                     <Link  href="https://www.instagram.com/kienletrung304/">
-                      <a>
+                      
                           <img className="h-6 w-6 fill-current text-blue-600 mr-6" src="https://img.icons8.com/windows/32/000000/instagram-new.png"/>
-                      </a>
+                     
                     </Link>
                     <Link href="#">
-                      <a >
+                     
                           <img className="h-6 w-6 fill-current text-blue-600 mr-6" src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-gmail-social-media-justicon-flat-justicon.png"/>
-                      </a>
+                     
                     </Link>
                     <Link  href="https://github.com/letrungkienqhh">
-                      <a>
+                   
                           <img className="h-6 w-6 fill-current text-blue-600 mr-6"  src="https://img.icons8.com/material-outlined/24/000000/github.png"/>
-                      </a>
+                      
                     </Link>
                 </div>
         </div>
